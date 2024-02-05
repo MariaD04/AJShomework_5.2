8dcec8b79325506551f181d02705e2010b089500
Mariya <maria_dolgova@inbox.ru>
2024-02-05 13:48:50 +0400

    Update classes

--- src/js/character.js
+++ src/js/character.js
@@ -4,8 +4,8 @@ export default class Character {
     this.getType(type);
     this.health = 100;
     this.level = 1;
-    this.attack = 0;
-    this.defence = 0;
+    this.attack = undefined;
+    this.defence = undefined;
   }
 
   getName(value) {
